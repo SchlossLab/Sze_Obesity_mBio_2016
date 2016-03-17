9ba9443b17518f41003c1627ad0be7fac44ee813
marcsze <marcsze@med.umich.edu>
2016-03-17 15:53:26 -0400

    R functions update

--- code/UsedFunctions.R
+++ code/UsedFunctions.R
@@ -3,12 +3,92 @@
 ## Marc Sze
 ## March 16, 2016
 
+## Function to make table for RR Analysis and run RR Test.
+## Needed Libraries include epiR.  
+## An Example of how to use:
+  ## RunRR(alpha.test, demographics, "obese", "H")
+RunRR <- function(testData, demo, testVar1, testVar2){
+  
+  # Check for necessary Libraries and load in not already attached
+  deps = c("epiR");
+  for (dep in deps){
+    if (dep %in% installed.packages()[,"Package"] == FALSE){
+      install.packages(as.character(dep), quiet=TRUE);
+    }
+    library(dep, verbose=FALSE, character.only=TRUE)
+  }
+  
+  # Generate categorical Data
+  Var1 <- demo[testVar1]
+  
+  # Generate Median Categorical Data
+  VarOfInt <- testData[testVar2]
+  VarOfInt <- as.data.frame(within(VarOfInt, 
+                     {Var.cat = ifelse(
+                       VarOfInt[,1] <= median(VarOfInt[,1]), 
+                       "less", "higher")}))
+  VarOfInt <- cbind(VarOfInt, Var1)
+  VarOfInt <- VarOfInt[order(VarOfInt[, 2]), ]
+  
+  # Generate Test table
+  totalN <- length(VarOfInt[, 2])
+  HighTotal <- length(which(VarOfInt[, 2]=="higher"))
+  HighMedianGroup <- as.data.frame(table(
+    VarOfInt[c(1:HighTotal), 3]))
+  LowMedianGroup <- as.data.frame(table(
+    VarOfInt[c((HighTotal + 1):totalN), 3]))
+  ConGroup <- c(HighMedianGroup[2, 2], HighMedianGroup[1, 2])
+  TestGroup <- c(LowMedianGroup[2, 2], LowMedianGroup[1, 2])
+  r.test <- rbind(TestGroup, ConGroup)
+  colnames(r.test) <- c("Yes", "No")
+  rownames(r.test) <- c("TestGroup", "ConGroup")
+  
+  # Run test and store important variables
+  EpiTest <- epi.2by2(r.test, method="cohort.count")
+  MassocTest <- EpiTest$massoc
+  RRValues <- MassocTest$RR.strata.score
+  RRsignif <- MassocTest$chisq.strata
+  
+  # Create Other variables to store for future use if needed
+  tpos <- LowMedianGroup[2, 2]
+  tneg <- LowMedianGroup[1, 2]
+  cpos <- HighMedianGroup[2, 2]
+  cneg <- HighMedianGroup[1, 2]
+  RR <- RRValues[1,1]
+  lowCI <- RRValues[1,2]
+  highCI <- RRValues[1,3]
+  
+  output <- list(totalN, tpos, tneg, cpos, cneg, RR, lowCI, highCI, RRsignif[1, 3])
+  names(output) <- c("totalN", "tpos", "tneg", "cpos", "cneg", 
+                     "RR", "lowCI", "highCI", "pValue")
+  
+  return(output)
+  
+}
+
+## Function to get the Simulated Power of Study.
+## Need to make sure that the TestData and demo tables rownames match
+## Example on how to use:
+## NonParaPowerSim(alpha.test, s1.metadata, "H", "obese", n=1000)
+NonParaPowerSim <- function(TestData, demo, Var1, Var2, n=1000){
+  keep <- which(demo[Var2] == "Yes")
+  TestData <- TestData[Var1]
+  VarOfInt <- TestData[, 1]
+  set.seed(3)
+  PvalSim <- replicate(n, wilcox.test(
+    rnorm(length(VarOfInt[keep]), mean(VarOfInt[keep]), sd(VarOfInt[keep])), 
+    rnorm(length(VarOfInt[-keep]), mean(VarOfInt[-keep]), 
+          sd(VarOfInt[-keep])))$p.value)
+  return((sum(PvalSim < 0.05)) / 10)
+}
 
 ## Function to graph OTU versus BMI linear regression with log scale option
-## Default for log is FALSE. 
-## Example:
+## Default for log is FALSE. Necessary library is ggplot2. 
+## Example on how to use:
 ## q1 <- ggBaseLinear(RossMicrobiome, RossDemo, "Otu00102", log=TRUE)
 ggBaseLinear <- function(microbiome, demo, otuNum, log=FALSE){
+  
+  # Check for needed library and load if necessary
   deps = c("ggplot2");
   for (dep in deps){
     if (dep %in% installed.packages()[,"Package"] == FALSE){
@@ -17,6 +97,7 @@ ggBaseLinear <- function(microbiome, demo, otuNum, log=FALSE){
     library(dep, verbose=FALSE, character.only=TRUE)
   }
   
+  # Run Log or normal linear regression
   if(log==FALSE){
     OTUData <- as.data.frame(microbiome[otuNum])
   } else{
@@ -31,10 +112,11 @@ ggBaseLinear <- function(microbiome, demo, otuNum, log=FALSE){
 
 
 ## Function to graph OTU versus BMI  boxplot with log scale option
-## Default for log is FALSE. 
-## Example:
+## Default for log is FALSE. Necessary library is ggplot2 
+## Example on how to use:
 ## q1 <- ggBaseLinear(RossMicrobiome, RossDemo, "Otu00102", "Ross", log=TRUE)
 ggBaseBox <- function(microbiome, demo, otuNum, title, log=FALSE){
+  # Check for needed libraries and load if necessary
   deps = c("ggplot2", "RColorBrewer");
   for (dep in deps){
     if (dep %in% installed.packages()[,"Package"] == FALSE){
@@ -43,6 +125,7 @@ ggBaseBox <- function(microbiome, demo, otuNum, title, log=FALSE){
     library(dep, verbose=FALSE, character.only=TRUE)
   }
   
+  # Run log or normal box plot graph
   if(log==FALSE){
     OTUData <- as.data.frame(microbiome[otuNum])
   } else{
