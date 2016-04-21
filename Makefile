37bbd7fe6fc9b2bb988b3edacbf3427b14f1e895
Pat Schloss <pschloss@umich.edu>
2016-04-21 15:33:28 -0400

    Generate necessary files for Baxter and Ross

--- Makefile
+++ Makefile
@@ -60,11 +60,11 @@ $(REFS)/trainset14_032015.% :
 
 
 # Get Baxter study data from project repository...
-
-data/baxter/baxter.normal.0.03.subsample.shared\
-	data/baxter/baxter.normal.0.03.subsample.braycurtis.0.03.lt.ave.dist\
-	data/baxter/baxter.normal.metadata\
-	data/baxter/baxter.taxonomy : code/baxter.batch code/baxter.R  
+data/baxter/baxter.braycurtis.0.03.lt.ave.dist\
+	data/baxter/baxter.groups.ave-std.summary
+	data/baxter/baxter.0.03.subsample.shared
+	data/baxter/baxter.metadata
+	data/baxter/baxter.taxonomy : code/baxter.batch code/baxter.R
 	bash code/baxter.batch
 
 
@@ -77,8 +77,11 @@ data/hmp/hmp.0.03.subsample.shared\
 
 
 # Get Ross study data and process through mothur
-
-ross : code/ross.batch
+data/ross/ross.braycurtis.0.03.lt.ave.dist\
+	data/ross/ross.groups.ave-std.summary
+	data/ross/ross.0.03.subsample.shared
+	data/ross/ross.metadata
+	data/ross/ross.taxonomy : code/ross.batch code/ross.R
 	bash code/ross.batch
 
 
---
+++ code/ross.R
@@ -0,0 +1,41 @@
+# Output shared file that has the same rows as the metadata file. Requirements:
+#		* Rows of shared and metadata files must be in the same order
+#		* Metadata file must contain sample id, gender (m/f), bmi, age,
+#			white(logical), obese(logical)
+
+
+metadata <- read.csv("data/ross/s40168-015-0072-y-s1.csv", stringsAsFactors=F)
+
+# [1] "sampleID"          "barcode"           "proximal"
+# [4] "distal"            "v_region"          "HbA1C"
+# [7] "sex"               "age_at_visit"      "height"
+#[10] "weight"            "BMI"               "sequence_run"
+#[13] "ALT"               "HOMA"              "experiment_center"
+#[16] "sample_type"       "Description"       "waist"
+#[19] "hip"               "arm_circ"          "pulse2"
+#[22] "labid"             "cholesterol"       "triglycerides"
+#[25] "hdl_chol"          "ldl_chol"          "crp"
+#[28] "mmol_gluc"         "homa_ir"           "homa_beta"
+
+keep <- c("sampleID", "sex", "BMI", "age_at_visit")
+md_keep <- metadata[,colnames(metadata) %in% keep]
+md_keep$obese <- md_keep$BMI >= 34.0
+md_keep$white <- FALSE
+
+lookup <- read.csv("data/ross/Hispanic_dataset.csv", stringsAsFactors=F)
+sample <- lookup[,"Run_s"]
+names(sample) <- lookup$Sample_Name_s
+
+md_keep$sampleID <- sample[md_keep$sampleID]
+colnames(md_keep) <- c("sample", "gender", "age", "bmi", "obese", "white")
+md_keep <- md_keep[order(md_keep$sample), ]
+
+shared <- read.table("data/ross/ross.unique.good.filter.unique.precluster.pick.pick.an.unique_list.shared", header=T)
+shared <- shared[order(shared$Group), ]
+
+stopifnot(md_keep$sample == shared$Group)
+
+
+write.table(md_keep, file="data/ross/ross.metadata", quote=F, sep='\t', row.names=F)
+
+write.table(shared, file="data/ross/ross.shared", quote=F, sep='\t', row.names=F)
