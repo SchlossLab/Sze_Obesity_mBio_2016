ea536f78889c6cf870556c1c11df805e34a4a9e0
Pat Schloss <pschloss@umich.edu>
2016-05-05 09:06:07 -0400

    Add random effects model to zscore analysis

--- code/run_zscore_analysis.R
+++ code/run_zscore_analysis.R
@@ -1,6 +1,14 @@
 source('code/utilities.R')
+get_dependencies("lme4")
 
-datasets <- c('baxter', 'escobar', 'hmp', 'ross', 'turnbaugh', 'wu', 'zupancic')
+datasets <- c('baxter', 'goodrich', 'escobar', 'hmp', 'ross', 'turnbaugh', 'wu', 'zupancic')
+
+re_model <- function(data, study, is_obese){
+	null_model <- lmer(data ~ (1|study), REML=FALSE)
+	re_model <- lmer(data ~ (1|is_obese) + (1|study), REML=FALSE)
+	test <- anova(null_model, re_model)
+	test$Pr[2]
+}
 
 run <- function(datasets){
 
@@ -24,21 +32,24 @@ run <- function(datasets){
 		bf_relabund <- get_bacteroides_firmicutes(d)
 
 		shannon_z <- scale(alpha$shannon)
+		sobs_z <- scale(alpha$sobs)
+		shannoneven_z <- scale(alpha$shannoneven)
+		b_z <- scale(log(bf_relabund[,"b"] + 1))
+		f_z <- scale(log(bf_relabund[,"f"] + 1))
 		bf_z <- scale(log(bf_relabund[,"bf"] + 1))
+
 		is_obese <- metadata$obese
 
-		z_transform <- rbind(z_transform, data.frame(study=d, shannon=shannon_z, bf=bf_z, obese=is_obese))
+		z_transform <- rbind(z_transform, data.frame(study=d, shannon=shannon_z, sobs=sobs_z, shannoneven=shannoneven_z, bacteroidetes=b_z, firmicutes=f_z, bf_ratio=bf_z, obese=is_obese))
 	}
 
 	write.table(z_transform, file="data/process/z_transform.data", quote=F, sep='\t', row.names=F)
 
-	shannon_test <- t.test(z_transform[,"shannon"]~z_transform[,"obese"])
-	bf_test <- t.test(z_transform[,"bf"]~z_transform[,"obese"])
+	metrics <- c("shannon", "sobs", "shannoneven", "bacteroidetes", "firmicutes", "bf_ratio")
 
-	test_summary <- matrix(c(shannon_test$estimate, shannon_test$p.value,
-									bf_test$estimate, bf_test$p.value), nrow=2, byrow=T)
-	rownames(test_summary) <- c("shannon", "bf")
-	colnames(test_summary) <- c("non-obese", "obese", "p.value")
+	p_values <- apply(z_transform[,metrics], 2, re_model, study=z_transform$study, is_obese=z_transform$obese)
 
-	write.table(test_summary, file="data/process/z_transform.test", quote=F, sep='\t', row.names=T)
+	test_summary <- data.frame(metric=names(p_values), p_value=p_values)
+
+	write.table(test_summary, file="data/process/z_transform.test", quote=F, sep='\t', row.names=F)
 }
