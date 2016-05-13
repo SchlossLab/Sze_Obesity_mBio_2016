0f3b52499714654685564a3f5746a06a44385b9f
Pat Schloss <pschloss@umich.edu>
2016-05-13 13:52:44 -0400

    Create plots of alpha diversity

---
+++ code/plot_alpha_diversity.R
@@ -0,0 +1,81 @@
+make_study_label <- function(data){
+	options(scipen=10000)
+	study <- ifelse(data$dataset=="hmp", "HMP", capwords(data$dataset))
+	p_value <- paste0("=", format(round(data$p.value, digits=3)))
+	p_value[p_value == "=0.000"] <- "<0.001"
+	study_label <- paste(study, " (P", p_value, ")", sep="")
+
+}
+
+make_metric_label <- function(metric){
+
+	formatted_metrics <- c(shannon = "Shannon Diversity Index",
+												shannoneven = "Shannon Evenness Index",
+												sobs = "Number of Observed OTUs",
+												bacteroidetes = "Relative Abundance of Bacteroidetes",
+												firmicutes = "Relative Abundance of Firmicutes",
+												bf_ratio = "Ratio of Bacteroidetes to Firmicutes"
+											)
+	formatted_metrics[metric]
+}
+
+capwords <- function(s, strict = FALSE) {
+		s <- as.character(s)
+		cap <- function(s) paste(toupper(substring(s, 1, 1)),
+									{s <- substring(s, 2); if(strict) tolower(s) else s},
+														 sep = "", collapse = " " )
+		sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
+}
+
+
+diversity_plot <- function(metric, leg=NULL){
+
+	gap <- 0.1
+
+	alpha <- read.table(file="data/process/alpha_tests.summary", header=T)
+	alpha$se_non <- alpha$sd_non/sqrt(alpha$n_non)
+	alpha$se_obese <- alpha$sd_obese/sqrt(alpha$n_obese)
+
+#	metric <- levels(alpha$metric)
+	datasets <- levels(alpha$dataset)
+
+
+	subset <- alpha[alpha$metric == metric,]
+	subset <- subset[order(subset$dataset),]
+	stopifnot(subset$dataset == datasets)
+
+
+	par(mar=c(5,8,0.5,0.5))
+	plot(NA, ylim=c(1,length(datasets)), xlim=c(0,max(subset$mean_non+1.95*subset$se_non, subset$mean_obese+1.95*subset$se_obese)), axes=F, xlab=make_metric_label(metric), ylab="")
+
+	abline(v=c(mean(subset$mean_non), mean(subset$mean_obese)), col=c("red", "blue"))
+
+	arrows(x0=subset$mean_obese, x1=subset$mean_obese+1.95*subset$se_obese, y0=length(datasets):1+gap, y1=length(datasets):1+gap, angle=90, length=gap/2)
+	arrows(x0=subset$mean_obese, x1=subset$mean_obese-1.95*subset$se_obese, y0=length(datasets):1+gap, y1=length(datasets):1+gap, angle=90, length=gap/2)
+
+	arrows(x0=subset$mean_non, x1=subset$mean_non+1.95*subset$se_non, y0=length(datasets):1-gap, y1=length(datasets):1-gap, angle=90, length=gap/2)
+	arrows(x0=subset$mean_non, x1=subset$mean_non-1.95*subset$se_non, y0=length(datasets):1-gap, y1=length(datasets):1-gap, angle=90, length=gap/2)
+
+	points(x=subset$mean_non, y=length(datasets):1-gap, pch=21, bg="red")
+	points(x=subset$mean_obese, y=length(datasets):1+gap, pch=21, bg="blue")
+
+
+	axis(1)
+	axis(2, at=length(datasets):1, label=make_study_label(subset), las=2, cex.axis=0.8)
+	box()
+
+	if(!is.null(leg)){
+		legend(x=leg[1], y=leg[2], legend=c("Non-obese", "Obese"), pch=21, pt.bg=c("blue", "red"), cex=0.8)
+	}
+
+}
+
+
+pdf(file="alpha.pdf")
+diversity_plot('shannon', leg=c(0,9))
+diversity_plot('shannoneven', leg=c(0,9))
+diversity_plot('sobs', leg=c(0,9))
+diversity_plot('bacteroidetes', leg=c(0,9))
+diversity_plot('firmicutes', leg=c(0,9))
+diversity_plot('bf_ratio', leg=c(4,9))
+dev.off()
