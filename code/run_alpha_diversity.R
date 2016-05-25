6a5c83437afe3c406ee471c743dce8f0d390aeba
Pat Schloss <pschloss@umich.edu>
2016-05-25 06:56:09 -0400

    Fix removal of NAs from bf_ratio data

--- code/run_alpha_diversity.R
+++ code/run_alpha_diversity.R
@@ -70,7 +70,7 @@ run <- function(datasets){
 		stopifnot(alpha$group == metadata$sample)
 
 		bf_relabund <- get_bacteroides_firmicutes(d)
-		bf_relabund <- bf_relabund[alpha$group,]
+		bf_relabund <- bf_relabund[!na_obesity,]
 		stopifnot(rownames(bf_relabund) == metadata$sample)
 
 
